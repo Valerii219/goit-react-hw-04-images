eff4d600080727a147a46e5f958c376f7665de88
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-07-23 17:38:29 +0200

    saveS

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,14 @@
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import css from 'styles.module.css';
 
-export function ImageGallery({propsImage, showModals }) {
+export function ImageGallery({propsImage, showModal }) {
   if (!propsImage) {
     return null;
   }
   return (
     <ul className={css.ImageGallery} >
       {propsImage.map((el) => (
-        <ImageGalleryItem key={el.id} id={el.id} webformatURL={el.webformatURL}  largeImageURL={el.largeImageURL} onClick={showModals} />
+        <ImageGalleryItem key={el.id} id={el.id} webformatURL={el.webformatURL}  largeImageURL={el.largeImageURL} onClick={showModal} />
       ))}
     </ul>
   );
